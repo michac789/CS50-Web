b2ea4eb3a07ce2ee7582dc202d1c02a589f51de5
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2021-12-15 15:10:00 +0800

    create initial models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Auction(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    category = models.CharField(max_length=32)
+
+class Bid(models.Model):
+    bidder = models.CharField(max_length=64)
+    price = models.IntegerField(max_length=32)
+    item = models.ForeignKey(Auction, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    name = models.CharField(max_length=64)
+    comment = models.CharField(max_length=256)
+    item = models.ForeignKey(Auction, on_delete=models.CASCADE)
