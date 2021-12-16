6d1256f7a0dbc9afe55f20c9ec927531a6a2b588
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2021-12-16 11:54:33 +0800

    update models

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin.filters import ListFilter
+from .models import Auction, Bid, Comment
 
 # Register your models here.
+
+class AuctionAdmin(admin.ModelAdmin):
+    pass #to add (?)
+    
+class BidAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Auction)
+admin.site.register(Bid)
+admin.site.register(Comment)
--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -6,14 +6,31 @@ class User(AbstractUser):
     pass
 
 class Auction(models.Model):
+    categories = (
+        ('FA', 'Fashion'),
+        ('TO', 'Toys'),
+        ('EL', 'Electronics'),
+        ('HO', 'Home'),
+        ('AR', 'Arts'),
+        ('SU', 'Supplies'),
+        ('ED', 'Education'),
+        ('MI', 'Miscellaneous')
+    )
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=256)
-    category = models.CharField(max_length=32)
+    category = models.CharField(max_length=32, choices=categories)
+    owner = models.ManyToManyField(User)
+    
+    def __str__(self):
+        return f"{self.title} ({self.category}): {self.description}"
 
 class Bid(models.Model):
-    bidder = models.CharField(max_length=64)
     price = models.IntegerField(max_length=32)
     item = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.bidder}: {self.item}, {self.price}"
 
 class Comment(models.Model):
     name = models.CharField(max_length=64)
